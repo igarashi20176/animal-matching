18a847fbf3c1891ba143a5300fcef98b6593d12d
igarashi20176 <c0a20176bc@edu.teu.ac.jp>
2022-09-27 23:47:36 +0900

    b

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Chart from '../views/ChartList.vue'
 import Animal from "../views/AnimalList.vue";
-import Detail from "../views/AnimalListDetail.vue";
+import Detail from "../templates/AnimalListDetail.vue";
 
 const routes = [
     {
