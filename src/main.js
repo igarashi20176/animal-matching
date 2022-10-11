6572df0d7e1f47f10a64d8ba3a7f5bcc5ea018ca
igarashi20176 <c0a20176bc@edu.teu.ac.jp>
2022-10-12 01:01:51 +0900

    a

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/css/main.css'
-import store from "./store/store";
 import router  from "./router/router";
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).mount('#app')
