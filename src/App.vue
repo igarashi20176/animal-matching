5d23f4de3ca0975c88f8aac17607be2ab69fea22
igarashi20176 <c0a20176bc@edu.teu.ac.jp>
2022-10-28 23:48:07 +0900

    a

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
   <the-routing-btn-col3 :btn-a="routingBtns[0]" :btn-b="routingBtns[1]" :btn-c="routingBtns[2]" />
 
-  <div class="relative scroll-none h-[450px] w-[800px] overflow-auto border-2 border-gray-400 rounded-2xl m-auto p-2 bg-[#fff]">
+  <div class="relative scroll-none h-[450px] w-[800px] m-auto p-2 bg-[#fff] overflow-auto border-2 border-gray-400 rounded-2xl">
     <router-view />
   </div>
 
