a3852e455de79898bf601d720ee914abbd624176
igarashi20176 <c0a20176bc@edu.teu.ac.jp>
2023-01-26 14:37:33 +0900

    change UI

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  // config
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
